8031324e1822d72514f366a322a00a6b3f58e204
Ruslan Orlov <org1978org@gmail.com>
2024-01-15 23:43:59 +0600

    Commit SQL Script another change

--- Scripts/Script-2.sql
+++ Scripts/Script-2.sql
@@ -5,6 +5,6 @@ select * from public.ingredients i order by id ;
 
 select * from public.users u ;
 
-select * from public.roles r ; 
+select * from public.roles r ;
 
 select * from public.users_roles ur ;
