1baa9c40459a0f02301096e414415a3399f88824
Ruslan Orlov <org1978org@gmail.com>
2024-08-23 11:16:25 +0500

    Commit the changes of support images on the server side #1 2024-08-23

--- src/main/java/recipes/dtos/RecipeDTO.java
+++ src/main/java/recipes/dtos/RecipeDTO.java
@@ -1,6 +1,5 @@
 package recipes.dtos;
 
-import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,7 +14,6 @@ public class RecipeDTO {
 	private Long id;
 	private String name;
 	private String description;
-	@Lob
 	private byte[] image;		/* Поддержка изображений */
 	private String imageUrl;	/* Поддержка изображений */
 	
